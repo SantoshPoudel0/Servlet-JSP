c5585787e855410205c455e7a5106335f245c9e3
SantoshPoudel0 <xpoudel5@gmail.com>
2026-01-15 17:48:14 +0545

    creating the login and register dynamic url with webxml

---
+++ src/main/java/np/sp/backend/login.java
@@ -0,0 +1,18 @@
+package np.sp.backend;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class login  extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("This is a login server code");
+		
+	}
+
+}
